50caa4a61ccd5ea933eb72b6d36e79543433e731
Davies Brown <nwosudavid77@gmail.com>
2023-12-09 22:53:58 +0100

    chore: Updated getStats to report redis state

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,11 +1,14 @@
 const dbClient = require('../utils/db');
+const redisClient = require('../utils/redis');
 
 const getStatus = (req, res) => {
+  // GET /status
   res.status(200).json({ status: 'Server is running' });
 };
 
 const getStats = (req, res) => {
-  res.status(200).json({ db: dbClient.isAlive() });
+  // GET /stats
+  res.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
 };
 
 module.exports = { getStatus, getStats };
