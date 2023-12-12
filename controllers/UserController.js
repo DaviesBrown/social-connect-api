50b8afeb3bfb62fee9d7e532b8da7ff6e732b110
Davies Brown <nwosudavid77@gmail.com>
2023-12-12 21:53:28 +0100

    feat: Update user account by id

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const User = require('../models/userModel');
 
 const getMe = async (req, res) => {
@@ -10,7 +11,29 @@ const getMe = async (req, res) => {
     res.status(500).json(error);
   }
 };
-const updateMe = async (req, res) => {};
+const updateMe = async (req, res) => {
+  // PUT /users/:id
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (err) {
+        return res.status(500).json(err);
+      }
+    }
+    try {
+      await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json('Your account has been updated');
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json('You can update only your account!');
+  }
+};
 const deleteMe = async (req, res) => {};
 const putFollow = async (req, res) => {};
 const putUnfollow = async (req, res) => {};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,9 +7,9 @@ const router = express.Router();
 
 // UserController routes
 router.get('/:id', authenticateToken, userController.getMe);
-router.put('/users/:id', authenticateToken, userController.updateMe);
-router.delete('/users/:id', authenticateToken, userController.deleteMe);
-router.put('/users/:id/follow', authenticateToken, userController.putFollow);
-router.put('/users/:id/unfollow', authenticateToken, userController.putUnfollow);
+router.put('/:id', authenticateToken, userController.updateMe);
+router.delete('/:id', authenticateToken, userController.deleteMe);
+router.put('/:id/follow', authenticateToken, userController.putFollow);
+router.put('/:id/unfollow', authenticateToken, userController.putUnfollow);
 
 module.exports = router;
