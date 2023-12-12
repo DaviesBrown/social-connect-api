d2c14092a03936bda32ebe9784f0cf17551bb521
Davies Brown <nwosudavid77@gmail.com>
2023-12-13 00:56:06 +0100

    feat: Liking and disliking a post

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -37,6 +37,7 @@ const updatePost = async (req, res) => {
 };
 
 const deletePost = async (req, res) => {
+  // DELETE /posts/:id
   try {
     const post = await Post.findById(req.params.id);
     if (post.userId === req.user.userId) {
@@ -50,9 +51,26 @@ const deletePost = async (req, res) => {
   }
 };
 
-const putLike = async (req, res) => {};
+const putLike = async (req, res) => {
+  // PUT /posts/:id/like
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post.likes.includes(req.user.userId)) {
+      await post.updateOne({ $push: { likes: req.user.userId } });
+      res.status(200).json('The post has been liked');
+    } else {
+      await post.updateOne({ $pull: { likes: req.user.userId } });
+      res.status(200).json('The post has been disliked');
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
 
-const putUnlike = async (req, res) => {};
+const getTimeline = async (req, res) => {
+  // GET /posts/timeline/all
+  
+};
 
 module.exports = {
   createPost, getPost, updatePost, deletePost, putLike, putUnlike,
