363d7cd0323825307d77b9c04159e5e4ccd7fff1
Davies Brown <nwosudavid77@gmail.com>
2023-12-13 00:49:10 +0100

    feat: Updating a post route

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -6,8 +6,8 @@ const createPost = async (req, res) => {
   try {
     const savedPost = await newPost.save();
     res.status(200).json(savedPost);
-  } catch (err) {
-    res.status(500).json(err);
+  } catch (error) {
+    res.status(500).json({ error });
   }
 };
 
@@ -16,7 +16,7 @@ const getPost = async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
     res.status(200).json(post);
-  } catch (err) {
+  } catch (error) {
     res.status(500).json({ error: 'Post not found' });
   }
 };
@@ -27,12 +27,12 @@ const updatePost = async (req, res) => {
     const post = await Post.findById(req.params.id);
     if (post.userId === req.user.userId) {
       await post.updateOne({ $set: req.body });
-      res.status(200).json('the post has been updated');
+      res.status(200).json({ message: 'The post has been updated' });
     } else {
-      res.status(403).json('you can update only your post');
+      res.status(403).json({ message: 'You can only update your post' });
     }
   } catch (error) {
-    res.status(500).json({error:  });
+    res.status(500).json({ error });
   }
 };
 
