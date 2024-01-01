d2d284083c909ac80a3371f716c1171c4649d119
Davies Brown <nwosudavid77@gmail.com>
2024-01-01 04:58:07 +0100

    feat: Added swaggerUI for the api documentation

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 /* eslint-disable jest/require-hook */
-const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv');
+const express = require('express');
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+
 const routes = require('./routes');
 const dbClient = require('./utils/db');
-const authenticateToken = require('./utils/auth');
 
 dotenv.config();
 
@@ -12,12 +15,43 @@ const PORT = process.env.PORT || 3000;
 
 dbClient.connectDB();
 
+app.use(cors());
 app.use(express.json());
-app.use('/', routes);
 
-app.get('/protected', authenticateToken, (req, res) => {
-  res.json({ message: 'Access granted' });
-});
+function logger(req, res, next) {
+  console.log(req.originalUrl);
+  next();
+}
+app.use('/', logger, routes);
+
+const options = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Social Connect API',
+      version: '1.0.0',
+      description: 'An api for connecting with people online',
+    },
+    components: {
+      securitySchemes: {
+        sessionAuth: {
+          type: 'apiKey',
+          in: 'header',
+          name: 'Authorization',
+        },
+      },
+    },
+    security: [{ sessionAuth: [] }],
+  },
+  apis: ['./routes/*.js'],
+};
+
+const specs = swaggerJSDoc(options);
+app.use(
+  '/api-docs',
+  swaggerUI.serve,
+  swaggerUI.setup(specs),
+);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
